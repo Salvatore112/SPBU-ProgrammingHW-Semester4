9127ecb8608e26d5905f1af11a878c1d150b5398
Pavel Averin <pasha2281337alfa@gmail.com>
2024-02-17 17:56:04 +0300

    Start working on the 1st task

---
+++ Task1/Triber_Stack/Program.cs
@@ -0,0 +1,11 @@
+﻿using Triber_Stack;
+
+var stack = new TriberStack();
+
+stack.Push(1);
+stack.Push(2);
+stack.Pop();
+
+Console.WriteLine(stack.Peek());
+
+Console.ReadKey();
---
+++ Task1/Triber_Stack/TriberStack.cs
@@ -0,0 +1,57 @@
+﻿namespace Triber_Stack;
+
+public class TriberStack
+{
+    private StackNode? H;
+    
+    private class StackNode
+    {
+        public StackNode? next { get; set; }
+        public int value { get; set; }
+
+        public StackNode(int value, StackNode nextNode)
+        {
+            this.next = nextNode;
+            this.value = value;
+        }
+
+        public StackNode() { }
+    }
+
+    public int Pop() 
+    {
+        while (true)
+        {
+            StackNode? head = H;
+            if (Interlocked.CompareExchange(ref H, head.next, head) != head.next)
+            {
+                return head.value;
+            }
+        }
+    }
+
+    public int Peek() => H.value;
+
+    public void Push(int newValue)
+    {
+        while (true) 
+        {  
+            StackNode? head = H;
+            if (head == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            var newHead = new StackNode(newValue, head);
+            if (Interlocked.CompareExchange(ref H, newHead, head) != newHead)
+            {
+                return;
+            }
+        }
+    }
+
+    public TriberStack()
+    {
+        this.H = new StackNode();
+    }
+}
